a5d33a09655656b3c64db9633d23d4fecb9a3a6d
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2023-04-24 09:13:44 +0200

    Created EMF and EM. Preparing persistence of 'Instruments'.

--- play-with-jpa/src/main/java/org/vitu/jpa/PlayWithMusiciens.java
+++ play-with-jpa/src/main/java/org/vitu/jpa/PlayWithMusiciens.java
@@ -1,5 +1,9 @@
 package org.vitu.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.vitu.jpa.model.Instrument;
 import org.vitu.jpa.model.util.TypeInstrument;
 
@@ -10,6 +14,13 @@ public class PlayWithMusiciens {
 		Instrument instrument = new Instrument("Violon", TypeInstrument.CORDES);
 		
 		System.out.println("Instrument = " + instrument);
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("play-with-jpa");
+		System.out.println("EMF = " + emf);
+		
+		EntityManager entityManager = emf.createEntityManager();
+		
+		entityManager.persist(instrument);
 
 	}
 }
