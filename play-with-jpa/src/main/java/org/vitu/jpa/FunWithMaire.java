9b2e825709d8c85bbbb7994d0eeb70f54e015f04
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2023-05-01 09:57:43 +0200

    End of the examples on the 'mutable conteners'.

--- play-with-jpa/src/main/java/org/vitu/jpa/FunWithMaire.java
+++ play-with-jpa/src/main/java/org/vitu/jpa/FunWithMaire.java
@@ -1,9 +1,32 @@
 package org.vitu.jpa;
 
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.vitu.jpa.model.Maire;
+import org.vitu.jpa.model.util.Civilite;
+
 public class FunWithMaire {
 
 	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("play-with-jpa");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
+		Date date = new Date(1952, 8, 4);
+		Maire maire = new Maire("Chirac", "Jacques", Civilite.M, date);
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(maire);
+		entityManager.getTransaction().commit();
+		
+		System.out.println("Maire = " + maire);
+		
+		date.setMonth(7);
+		
+		System.out.println("Maire = " + maire);
 	}
 
 }
